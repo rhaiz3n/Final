1582e4e3d19948c03e3dabee880bd23be33509b2
rhaiz3n <119270733+rhaiz3n@users.noreply.github.com>
2023-01-22 13:29:59 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link 
+            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" 
+        />
       </Head>
       <body>
         <Main />
